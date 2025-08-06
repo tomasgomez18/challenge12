274516b8928904a0ec3f1dd21ccd68b6e12f0c7a
tomas <tomasgomezzi36@gmail.com>
2025-08-06 15:20:39 -0300

    ejercicio 16 terminado

--- challenge12/16_1.js
+++ challenge12/16_1.js
@@ -1,1 +1,11 @@
 // Verificar si una contraseña tiene al menos 8 caracteres, incluye mayúsculas, números y símbolos.
+
+let clave = prompt("Ingrese una clave de al menos 8 caracteres");
+let cantidad = clave.length;
+
+
+if (clave.length >= 8) {
+  alert(`¡Clave aceptada!, tiene ${cantidad}  caracteres`);
+} else {
+  alert(`La clave es demasiado corta, tiene ${cantidad}  caracteres`);
+}

a3d396e803f253a0cc6682d2f16579dd14a51da6
tomas <tomasgomezzi36@gmail.com>
2025-08-06 14:22:56 -0300

    ejercicio 15 incompleto

---
+++ challenge12/15_1.js
@@ -0,0 +1,12 @@
+// Generar un número aleatorio entre 1 y 10, y verificar si el número ingresado por el usuario es mayor, menor o igual.
+let num = parseInt(prompt("Ingrese un numero del 1 al 10"))
+let aleatorio = Math.random(10)
+ if(num>aleatorio){
+    alert(`El numero ${num} es mayor que ${aleatorio}`)
+ }else if(num<aleatorio){
+    alert(`El numero ${num} es menor que ${aleatorio}`) 
+ }else if(num==aleatorio){
+    alert(`El numero ${num} es igual a ${aleatorio}`)
+ }else{
+    alert("opcion invalida")
+ }

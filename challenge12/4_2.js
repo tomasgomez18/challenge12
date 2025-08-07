e88e321f071fe8aa2a02c02d15a8d26080c14dd0
tomas <tomasgomezzi36@gmail.com>
2025-08-07 01:10:30 -0300

    ejercicio 4 pdf 2 termiando

---
+++ challenge12/4_2.js
@@ -0,0 +1,11 @@
+// Mostrar todos los pares entre 1 y 100
+
+let num = 0;
+
+
+for(let i = 0 ; i<100 ; i+=2){
+    if (num<=100){
+         num = num + 2
+    alert(`Los numero pares del 1 al 100 son: ${num}`);
+    }
+}

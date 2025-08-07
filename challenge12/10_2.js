555b616cfa44bb8569e65281a79cb4ccdec41e44
tomas <tomasgomezzi36@gmail.com>
2025-08-07 15:28:56 -0300

    ejercicio 10 pdf 2 terminado

---
+++ challenge12/10_2.js
@@ -0,0 +1,24 @@
+//Solicitar números y mostrar si están en orden creciente
+
+  let cantidad = parseInt(
+    prompt("¿Cuántos números vas a ingresar? (mínimo 2)")
+  );
+
+  let anterior = parseInt(prompt("Número 1:"));
+  let esCreciente = true;
+
+  for (let i = 2; i <= cantidad; i++) {
+    let actual = parseInt(prompt(`Número ${i}:`));
+
+    if (actual <= anterior) {
+      esCreciente = false;
+    }
+
+    anterior = actual;
+  }
+
+  if (esCreciente) {
+    alert("Los números están en orden creciente.");
+  } else {
+    alert("Los números NO están en orden creciente.");
+  }
